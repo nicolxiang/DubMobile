31270150e0bb9d4d7493b42bab9c8a2038ac8865
Nicol <nicol.xiang@missionsky.com>
2015-02-20 10:31:53 +0800

    nicol commit

--- apps/Citizen/iphone/native/www/default/worklight/checksum.js
+++ apps/Citizen/iphone/native/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":2847700506,"date":1424344345278,"machine":"asus-PC"};
-/* Date: Thu Feb 19 19:12:25 CST 2015 */
+var WL_CHECKSUM = {"checksum":1324933739,"date":1424399364509,"machine":"asus-PC"};
+/* Date: Fri Feb 20 10:29:24 CST 2015 */
