e18069a2c622092f5c6719ef08813ad8606ba732
Nicol <nicol.xiang@missionsky.com>
2015-02-20 22:57:19 +0800

    Fixed bug 2015 02 20

--- apps/Citizen/iphone/native/www/default/worklight/checksum.js
+++ apps/Citizen/iphone/native/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":1324933739,"date":1424399364509,"machine":"asus-PC"};
-/* Date: Fri Feb 20 10:29:24 CST 2015 */
+var WL_CHECKSUM = {"checksum":2208967068,"date":1424438086301,"machine":"asus-PC"};
+/* Date: Fri Feb 20 21:14:46 CST 2015 */
