c461def7e0619a6938a18ed7cb0200337061730e
gradylu <gradylu@163.com>
2015-02-23 15:39:25 +0800

    update style for service1.html

---
+++ apps/Citizen/common/js/controllers/Service2Controller.js
@@ -0,0 +1,7 @@
+citizenApp.controller('Service2Controller',function($scope, $http, $rootScope, $location) {
+	$scope.selectedPestType = "Medical Fitness Certificate(XT)";
+	
+	$scope.selectPestType = function(pestType){
+		$scope.selectedPestType = pestType;	
+	};
+});
