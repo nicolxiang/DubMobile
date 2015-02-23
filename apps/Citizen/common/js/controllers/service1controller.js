ec9a02649cea6d315ac3f3b6308812bc75126564
Nicol <nicol.xiang@missionsky.com>
2015-02-23 15:23:13 +0800

    resolve conflicts

--- apps/Citizen/common/js/controllers/service1controller.js
+++ apps/Citizen/common/js/controllers/service1controller.js
@@ -1,5 +1,10 @@
 /**
  * Created by nicol.xiang on 2015/2/16.
  */
-citizenApp.controller('service1Controller',[function( ) {
-}]);
+citizenApp.controller('Service1Controller',function($scope, $http, $rootScope, $location) {
+	$scope.selectedPestType = "Pest Type";
+	
+	$scope.selectPestType = function(pestType){
+		$scope.selectedPestType = pestType;	
+	};
+});
